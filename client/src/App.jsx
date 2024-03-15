e9c87644292c5c0414c4994d1b57de3face41778
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-03-15 11:39:08 +0530

    header updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
